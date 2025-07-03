01dd3a6ebb73fb62311352623933dddbb0d801f9
PrinBless <jothikrishnan500@gmail.com>
2025-07-03 19:43:25 +0530

    Add files via upload

---
+++ assets/js/left.js
@@ -0,0 +1,46 @@
+function lets(){
+  let city ="Chennai";
+ let current = "velachery";
+  console.log(city);
+  if(true){
+    let city="velachery";
+    console.log(city);
+  }
+  console.log(current);
+}
+
+
+lets();
+function constant(){
+  const movie="coolie";
+ 
+  console.log(movie);
+}
+constant();
+function elementvar(){
+
+var name=[1,2,3,4]
+if (true){
+console.log(name)
+}
+console.log(name)
+}
+elementvar();
+
+function elementlet(){
+let age=21;
+if (age==20){
+console.log("Hii")
+}
+}
+
+elementlet();
+function elementcons() {
+  const N = "jothi";
+  if (N == "prince") {
+    console.log("Name is prince");
+  } else {
+    console.log("Name is not prince");
+  }
+}
+elementcons();
